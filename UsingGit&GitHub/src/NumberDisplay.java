880918caf4b8c159aacfc71eab4680676dc8bfd2
Baipitt <Baipitt@192.168.1.4>
2016-08-05 10:30:40 +1200

    Added new function setValue with void return type

--- UsingGit&GitHub/src/NumberDisplay.java
+++ UsingGit&GitHub/src/NumberDisplay.java
@@ -10,6 +10,11 @@ public class NumberDisplay{
 	public int getValue(){
 		return value;
 	}
+	public void setValue(int replacementValue){
+		if((replacementValue >=0 ) && (replacementValue < limit)){
+			value = replacementValue;
+		}
+	}
 	public static void main(String[] args) {
 		System.out.println(".....");
 	}
