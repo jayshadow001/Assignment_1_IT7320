c45c4de0d6b3537f86e2d04713d4aeec19a56de3
Baipitt <Baipitt@192.168.1.4>
2016-08-05 10:42:21 +1200

    Added new Constructor, this constructor creates a new clock set at the time specified by the parameters.

--- UsingGit&GitHub/src/ClockDisplay.java
+++ UsingGit&GitHub/src/ClockDisplay.java
@@ -9,6 +9,18 @@ public class ClockDisplay {
 		updateDisplay();
 	}
 	
+	public ClockDisplay(int hour, int minute){
+		hours = new NumberDisplay(24);
+		minutes = new NumberDisplay(60);
+		setTime(hour, minute);
+	}
+	
+	public void setTime(int hour, int minute){
+		hours.setValue(hour);
+		minutes.setValue(minute);
+		updateDisplay();
+	}
+	
 	private void updateDisplay(){
 		displayString = hours.getDisplayValue() + ":" + minutes.getDisplayValue();
 	}
