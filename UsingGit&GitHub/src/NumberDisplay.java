1dbc73eecb40e3306e1e9f79e409dc3053ff1b94
Baipitt <Baipitt@192.168.1.4>
2016-08-05 09:16:07 +1200

    Added new function getValue with int return type.

---
+++ UsingGit&GitHub/src/NumberDisplay.java
@@ -0,0 +1,16 @@
+public class NumberDisplay{
+
+	private int limit;
+	private int value;
+	
+	public NumberDisplay(int rollOverLimit){
+		limit = rollOverLimit;
+		value = 0;
+	}
+	public int getValue(){
+		return value;
+	}
+	public static void main(String[] args) {
+		System.out.println(".....");
+	}
+}
